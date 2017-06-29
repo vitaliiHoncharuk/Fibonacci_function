59a1d5f0e462cddfeb0019c555048a61e3a41002
Vitalij <goncharukvitalij@gmail.com>
2017-06-29 12:54:04 +0000

    Initial

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 var k = Number(prompt("Please,input amount of Fibonacci numbers to calculate."));
 //-------------function using cycle
-function fibonacciCounter(amount_of_numbers) {
+function fibonacciCounter(amountOfNumbers) {
     var arr = [];
     arr[0] = 1;
     arr[1] = 1;
     document.write("<table border='1' cellpadding='20px'>")
-    for (var i = 2; i <= amount_of_numbers; i++) {
+    for (var i = 2; i <= amountOfNumbers; i++) {
         arr[i] = arr[i - 1] + arr[i - 2];
     }
     tr();
-    for (var i = 1; i <= amount_of_numbers; i++) {
+    for (var i = 1; i <= amountOfNumbers; i++) {
         if (i == 1) document.write("<th>Current Fibonacci Number</th>");
         document.write("<th>" + i + "</th>");
     }
     closeTr();
     tr();
-    for (var i = 0; i < amount_of_numbers; i++) {
+    for (var i = 0; i < amountOfNumbers; i++) {
         if (i == 0) document.write("<th>Numeric</th>");
         document.write("<td>" + arr[i] + "</td>");
     }
